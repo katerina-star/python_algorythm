273ca5b7aa4704c510728a5702961528043d62e3
katerina-star <70368178+katerina-star@users.noreply.github.com>
2021-02-15 17:03:03 +0300

    Add files via upload

--- task_6.py
+++ task_6.py
@@ -1,55 +1,33 @@
 """
-Задание 6.
-Задание на закрепление навыков работы с очередью
+6.	В программе генерируется случайное целое число от 0 до 100.
+Пользователь должен его отгадать не более чем за 10 попыток. После каждой
+неудачной попытки должно сообщаться больше или меньше введенное пользователем
+число, чем то, что загадано. Если за 10 попыток число не отгадано,
+то вывести загаданное число.
 
-Примечание: в этом задании вспомните ваши знания по работе с ООП
-и опирайтесь на пример урока
+Решите через рекурсию. Решение через цикл не принимается.
+Для оценки Отлично в этом блоке необходимо выполнить 5 заданий из 7
 
-Реализуйте структуру "доска задач".
-
-Структура должна предусматривать наличие несольких очередей задач, например
-1) базовой, откуда задачи берутся, решаются и отправляются в список решенных
-2) очередь на доработку, когда нерешенные задачи из первой очереди отправляются
-на корректировку решения
-
-После реализации структуры, проверьте ее работу на различных сценариях
+Подсказка:
+Базовый случай здесь - угадали число или закончились попытки
 """
 
-
-class TaskQueue:
-    def __init__(self):
-        self.base_queue = []
-        self.rework_queue = []
-        self.completed_tasks = []
-
-    def __str__(self):
-        base = list(map(str, self.base_queue))
-        rework = list(map(str, self.rework_queue))
-        completed = list(map(str, self.completed_tasks))
-        return f'Базовая очередь :{base} \nОчередь на переработку: {rework} \nВыполненные задачи: {completed}'
-
-    def add_task(self, name):
-        self.base_queue.append(name)
-
-    def task_to_rework(self):
-        self.rework_queue.append(self.base_queue.pop(0))
-
-    def task_back_to_base(self):
-        self.base_queue.append(self.rework_queue.pop(0))
-
-    def complete_task(self):
-        self.completed_tasks.append(self.base_queue.pop(0))
+import random
 
 
-tasks = TaskQueue()
+def recursion(i, num):
+    print(f'Попытка №{i}')
+    user_answer = int(input('Введите число от 0 до 100: '))
+    if i == 10 or user_answer == num:
+        if user_answer == num:
+            print('Вы угадали!')
+        print(f'Задуманное число: {num}')
+    else:
+        if user_answer > num:
+            print('Задуманное число меньше вашего')
+        else:
+            print('Задуманное число больше вашего')
+        recursion(i + 1, num)
 
-tasks.add_task('start')
-tasks.add_task('task1')
-tasks.add_task('task2')
-tasks.add_task('task4')
-tasks.add_task('finish')
 
-tasks.complete_task()
-tasks.complete_task()
-tasks.task_to_rework()
-print(tasks)
+recursion(1, random.randint(0, 100))
